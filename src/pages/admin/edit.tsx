c6813e5e3c63cbe4547b66b7835d47007875d600
Jake Brown <jake@jakebrown.net>
2025-04-04 21:39:19 +1030

    refactors blog post form into component

--- src/pages/admin/edit.tsx
+++ src/pages/admin/edit.tsx
@@ -1,11 +1,12 @@
 import { css } from "hono/css";
 import { useRequestContext } from "hono/jsx-renderer";
 import DurableDatabase from "../../db";
+import PostForm from "#src//components/postform.js";
 
 export default async function Page() {
   const ctx = useRequestContext<{ Bindings: Env }>();
-  const stub = DurableDatabase.getDefault(ctx.env);
   const slug = ctx.req.param("slug");
+  const stub = DurableDatabase.getDefault(ctx.env);
   const post = await stub.get(slug);
 
   return (
@@ -21,103 +22,9 @@ export default async function Page() {
           }
         `}
       >
-        <h1>Edit Post New</h1>
+        <h1>Edit Post</h1>
+        <PostForm post={post} />
 
-        <form
-          hx-put="/admin/hx-posts"
-          hx-trigger="submit"
-          hx-target="#response"
-          hx-target-error="#error"
-          hx-swap="innerHTML"
-          class={css`
-            display: grid;
-            gap: 10px;
-            grid-template-columns: 1fr 3fr 1fr 3fr 1fr 3fr;
-            label {
-              align-self: center;
-              text-align: right;
-            }
-            input {
-              margin: 0px;
-              font-size: 0.75rem;
-              line-height: 1rem;
-              padding: 5px;
-            }
-            input[type="checkbox"] {
-              align-self: center;
-            }
-            textarea {
-              margin: 0px;
-              font-size: 0.75rem;
-              line-height: 1rem;
-              grid-column: 1 / -1;
-            }
-
-            #response {
-              grid-column: 1 / -1;
-            }
-          `}
-        >
-          <label htmlFor="slug">Slug</label>
-          <input
-            type="text"
-            id="slug"
-            name="slug"
-            value={post.slug}
-            readOnly
-            style={{ cursor: "not-allowed" }}
-          />
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={post.title}
-            required
-          />
-
-          <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" value={post.date} required />
-
-          <label htmlFor="status">Status</label>
-          <select id="status" name="status" required>
-            <option
-              value="draft"
-              selected={post.status === "draft" ? true : false}
-            >
-              Draft
-            </option>
-            <option
-              value="unlisted"
-              selected={post.status === "unlisted" ? true : false}
-            >
-              Unlisted
-            </option>
-            <option
-              value="published"
-              selected={post.status === "published" ? true : false}
-            >
-              Published
-            </option>
-          </select>
-
-          <label htmlFor="tags">Tags</label>
-          <input
-            id="tags"
-            name="tags"
-            type="text"
-            value={post.tags || ""}
-          ></input>
-
-          <textarea id="introContent" name="introContent" rows={25} required>
-            {post.introContent}
-          </textarea>
-
-          <textarea id="moreContent" name="moreContent" rows={25}>
-            {post.moreContent}
-          </textarea>
-          <button type="submit">Save</button>
-        </form>
         <div id="error"></div>
         <div id="response"></div>
       </div>
--- src/pages/admin/new.tsx
+++ src/pages/admin/new.tsx
@@ -1,3 +1,4 @@
+import PostForm from "#src//components/postform.js";
 import { css } from "hono/css";
 
 export default async function Page() {
@@ -15,70 +16,8 @@ export default async function Page() {
         `}
       >
         <h1>New Post</h1>
+        <PostForm />
 
-        <form
-          hx-post="/admin/hx-posts"
-          hx-trigger="submit"
-          hx-target="#response"
-          hx-target-error="#error"
-          hx-swap="innerHTML"
-          class={css`
-            display: grid;
-            gap: 10px;
-            grid-template-columns: 1fr 3fr 1fr 3fr 1fr 3fr;
-            label {
-              align-self: center;
-              text-align: right;
-            }
-            input {
-              margin: 0px;
-              font-size: 0.75rem;
-              line-height: 1rem;
-              padding: 5px;
-            }
-            input[type="checkbox"] {
-              align-self: center;
-            }
-            textarea {
-              margin: 0px;
-              font-size: 0.75rem;
-              line-height: 1rem;
-              grid-column: 1 / -1;
-            }
-
-            #response {
-              grid-column: 1 / -1;
-            }
-          `}
-        >
-          <label htmlFor="slug">Slug</label>
-          <input type="text" id="slug" name="slug" required />
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" required />
-
-          <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" required />
-
-          <label htmlFor="status">Status</label>
-          <select id="status" name="status" required>
-            <option value="draft">Draft</option>
-            <option value="unlisted">Unlisted</option>
-            <option value="published">Published</option>
-          </select>
-
-          <label htmlFor="tags">Tags</label>
-          <input type="text" id="tags" name="tags"></input>
-
-          <textarea
-            id="introContent"
-            name="introContent"
-            rows={25}
-            required
-          ></textarea>
-
-          <textarea id="moreContent" name="moreContent" rows={25}></textarea>
-          <button type="submit">Create</button>
-        </form>
         <div id="error"></div>
         <div id="response"></div>
       </div>
